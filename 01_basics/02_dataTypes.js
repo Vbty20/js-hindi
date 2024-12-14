b04ff23a44f2c72001a167b83792f41fd438eb1b
Vbty20 <149299379+Vbty20@users.noreply.github.com>
2024-12-14 16:21:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+//code readability should be high
+console.log(3
+    + 3
+)
+
+ let name = "Vigoraj" //string;
+ let age = 21 
+ let isLoggedIn = false //boolean
+ let state;
+
+ //number => 2 to power 53 , is the range to store in number
+ //bigint for big numbers
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefined => 
+//symbol => to find uniqueness
+
+//object
+
+console.log(typeof "Vigoraj")
+console.log(typeof null)
+console.log(typeof undefined); // output is undefined
+
