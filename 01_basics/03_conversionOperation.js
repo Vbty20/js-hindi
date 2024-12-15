de3b51b5365d5e3e7dc57ec276135c6927c0e7fb
Vbty20 <149299379+Vbty20@users.noreply.github.com>
2024-12-15 04:56:09 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+//console.log(typeof(score))
+//console.log(typeof score);
+
+let valueInNumber = Number(score) //String(score) => converts to string
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+// "33" = 33
+//"33abc" = NaN
+// true => 1; false => 0
+// null => type is object and => 0
+// undefined => NaN
+
+let isLoggedIn = "vigoraj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "vigoraj" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNumber);
